313f53c2f7e156b94f408fc3cc7404f92e05ddc4
ljurak <lucas.jurak@gmail.com>
2020-06-18 17:22:56 +0200

    Add queries with purchase categories

--- src/main/java/budget/BudgetRepository.java
+++ src/main/java/budget/BudgetRepository.java
@@ -10,23 +10,54 @@ public class BudgetRepository {
     private List<BudgetItem> budgetItems = new ArrayList<>();
 
     public void addIncome(BigDecimal amount) {
-        budgetItems.add(new BudgetItem("Income", amount, BudgetItemType.INCOME));
+        budgetItems.add(new BudgetItem("Income", amount, BudgetItemType.INCOME, null));
     }
 
-    public void addPurchase(String name, BigDecimal amount) {
-        budgetItems.add(new BudgetItem(name, amount, BudgetItemType.PURCHASE));
+    public void addPurchase(String name, BigDecimal amount, PurchaseCategory category) {
+        budgetItems.add(new BudgetItem(name, amount, BudgetItemType.PURCHASE, category));
     }
 
     public List<BudgetItem> getPurchases() {
-        return  budgetItems.stream()
+        return budgetItems.stream()
                 .filter(item -> item.getType() == BudgetItemType.PURCHASE)
                 .collect(Collectors.toList());
+    }
+
+    public List<BudgetItem> getPurchasesByCategory(PurchaseCategory category) {
+        if (category == PurchaseCategory.ALL) {
+            return getPurchases();
+        }
+
+        return budgetItems.stream()
+                .filter(item -> item.getType() == BudgetItemType.PURCHASE)
+                .filter(item -> item.getCategory() == category)
+                .collect(Collectors.toList());
+    }
+
+    public long getPurchasesCount() {
+        return budgetItems.stream()
+                .filter(item -> item.getType() == BudgetItemType.PURCHASE)
+                .count();
     }
 
     public BigDecimal getTotalPurchases() {
         BigDecimal total = new BigDecimal(0);
         for (BudgetItem item : budgetItems) {
             if (item.getType() == BudgetItemType.PURCHASE) {
+                total = total.add(item.getAmount());
+            }
+        }
+        return total;
+    }
+
+    public BigDecimal getTotalPurchasesByCategory(PurchaseCategory category) {
+        if (category == PurchaseCategory.ALL) {
+            return getTotalPurchases();
+        }
+
+        BigDecimal total = new BigDecimal(0);
+        for (BudgetItem item : budgetItems) {
+            if (item.getType() == BudgetItemType.PURCHASE && item.getCategory() == category) {
                 total = total.add(item.getAmount());
             }
         }

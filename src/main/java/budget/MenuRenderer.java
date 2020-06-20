dbfcd5452d578efb572aa00745860c929a481c75
ljurak <lucas.jurak@gmail.com>
2020-06-20 23:51:43 +0200

    Add class for displaying menu

---
+++ src/main/java/budget/MenuRenderer.java
@@ -0,0 +1,51 @@
+package budget;
+
+public class MenuRenderer {
+
+    public void showMainMenu() {
+        System.out.println("Choose your action:");
+        System.out.println("1) Add income");
+        System.out.println("2) Add purchase");
+        System.out.println("3) Show list of purchases");
+        System.out.println("4) Balance");
+        System.out.println("5) Save");
+        System.out.println("6) Load");
+        System.out.println("7) Analyze (Sort)");
+        System.out.println("0) Exit");
+    }
+
+    public void showAddPurchaseCategories() {
+        System.out.println("Choose the type of purchase:");
+        System.out.println("1) Food");
+        System.out.println("2) Clothes");
+        System.out.println("3) Entertainment");
+        System.out.println("4) Other");
+        System.out.println("5) Back");
+    }
+
+    public void showSearchPurchaseCategories() {
+        System.out.println("Choose the type of purchase:");
+        System.out.println("1) Food");
+        System.out.println("2) Clothes");
+        System.out.println("3) Entertainment");
+        System.out.println("4) Other");
+        System.out.println("5) All");
+        System.out.println("6) Back");
+    }
+
+    public void showSortPurchaseCategories() {
+        System.out.println("Choose the type of purchase:");
+        System.out.println("1) Food");
+        System.out.println("2) Clothes");
+        System.out.println("3) Entertainment");
+        System.out.println("4) Other");
+    }
+
+    public void showSortOptions() {
+        System.out.println("How do you want to sort?");
+        System.out.println("1) Sort all purchases");
+        System.out.println("2) Sort by type");
+        System.out.println("3) Sort certain type");
+        System.out.println("4) Back");
+    }
+}

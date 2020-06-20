7a7f172c4ffc3375019952f9e15c3f3201eb1b0e
ljurak <lucas.jurak@gmail.com>
2020-06-20 23:53:28 +0200

    Add category property and method valueOfCategory

--- src/main/java/budget/PurchaseCategory.java
+++ src/main/java/budget/PurchaseCategory.java
@@ -1,16 +1,29 @@
 package budget;
 
 public enum PurchaseCategory {
-    FOOD("Food"),
-    CLOTHES("Clothes"),
-    ENTERTAINMENT("Entertainment"),
-    OTHER("Other"),
-    ALL("All");
+
+    FOOD("Food", 1),
+    CLOTHES("Clothes", 2),
+    ENTERTAINMENT("Entertainment", 3),
+    OTHER("Other", 4),
+    ALL("All", 5);
 
     private String name;
 
-    PurchaseCategory(String name) {
+    private int category;
+
+    PurchaseCategory(String name, int category) {
         this.name = name;
+        this.category = category;
+    }
+
+    public static PurchaseCategory valueOfCategory(int value) {
+        for (PurchaseCategory category : values()) {
+            if (category.category == value) {
+                return category;
+            }
+        }
+        return null;
     }
 
     @Override
